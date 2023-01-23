1fcd448b614601a2744071e2523e043d9b1d91ad
Tono Villarejo Reig <tonovillarejo@gmail.com>
2023-01-23 19:02:34 +0100

    Guardando_str_no_funciona_redundancia

--- cllient_str.c
+++ cllient_str.c
@@ -6,7 +6,7 @@
 /*   By: tvillare <tvillare@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/22 15:43:40 by tvillare          #+#    #+#             */
-/*   Updated: 2023/01/23 14:52:01 by tvillare         ###   ########.fr       */
+/*   Updated: 2023/01/23 18:57:56 by tvillare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,12 @@ static void	sig_usr(int signo, siginfo_t *info, void *context)
 static int	valid_pid(char *str)
 {
 	int	count;
+	struct sigaction	sa;
 
+	sa.sa_flags = SA_SIGINFO;
+	sa.sa_sigaction = sig_usr;
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
 	count = -1;
 	while (str[++count] != 0)
 		if (str[count] < 48 || str[count] > 57)
@@ -39,30 +44,11 @@ static int	ft_killed(char chr, int pid)
 	int	count;
 
 	count = 0;
+	ft_printf("%c", chr);
 	while (8 > count)
 	{
 		usleep(300);
 		if (chr & 128)
-			kill(pid, SIGUSR1);
-		else
-			kill(pid, SIGUSR2);
-		chr = chr & 127;
-		chr = chr << 1;
-		count++;
-	}
-	return (1);
-}
-static void	put_len(int len, int pid)
-{
-	int		count;
-	long	elevar;
-
-	elevar = 1;
-	count = 0;
-	while (31 > count)
-	{
-		usleep(300);
-		if (len & elevar)
 		{
 			ft_printf("1");
 			kill(pid, SIGUSR1);
@@ -72,13 +58,97 @@ static void	put_len(int len, int pid)
 			ft_printf("0");
 			kill(pid, SIGUSR2);
 		}
+		chr = chr & 127;
+		chr = chr << 1;
+		count++;
+	}
+	ft_printf("\n");
+	return (1);
+}
+
+static void	put_len(int len, int pid)
+{
+	int		count;
+	long	elevar;
+	struct sigaction	sa;
+
+	sa.sa_flags = SA_SIGINFO;
+	sa.sa_sigaction = sig_usr;
+
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	elevar = 1;
+	count = 0;
+	while (31 > count)
+	{
+		usleep(300);
+		if (len & elevar)
+		{
+			ft_printf("1");
+			if (kill(pid, SIGUSR1) != 0)
+				exit(1);
+		}
+		else
+		{
+			ft_printf("0");
+			if (kill(pid, SIGUSR2) != 0)
+				exit(1);
+		}
 		//len = chr & 127;
 		//len = len >> 1;
 		elevar *= 2;
 		count++;
 	}
-	//return (1);
+
 }
+
+/*
+static void	ft_send_char(int pid_server, char letter)
+{
+	int		aux;
+	int		k;
+
+	k = 0;
+	aux = letter;
+	while (++k <= 8)
+	{
+		if (aux % 2 == 0)
+		{
+			usleep(300);
+			if (kill(pid_server, SIGUSR2) == -1)
+				exit(EXIT_FAILURE);
+		}
+		else
+		{
+			usleep(300);
+			if (kill(pid_server, SIGUSR1) == -1)
+				exit(EXIT_FAILURE);
+		}
+		aux >>= 1;
+		//pause();
+	}
+}
+static void	ft_send_str(int pid_server, char *str)
+{
+	int					i;
+	struct sigaction	sa_confirm;
+
+	i = -1;
+	sa_confirm.sa_sigaction = &sig_usr;
+	sigaction(SIGUSR2, &sa_confirm, NULL);
+	while (str[++i])
+		ft_send_char(pid_server, str[i]);
+	i = -1;
+	while (++i < 8)
+	{
+		usleep(100);
+		if (kill(pid_server, SIGUSR2) == -1)
+			exit(EXIT_FAILURE);
+		//pause();
+	}
+}
+
+*/
 
 int	main(int argc, char **argv)
 {
@@ -89,6 +159,8 @@ int	main(int argc, char **argv)
 
 	sa.sa_flags = SA_SIGINFO;
 	sa.sa_sigaction = sig_usr;
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
 	if (argc != 3)
 	{
 		ft_printf("Error: Numero de parametros erroneo");
@@ -101,12 +173,14 @@ int	main(int argc, char **argv)
 	}
 	pid = atoi(argv[1]);
 	count = 0;
+
 	len = ft_strlen(argv[2]);
 	ft_printf("%d/", len);
 	put_len(len, pid);
-	sigaction(SIGUSR1, &sa, NULL);
-	sigaction(SIGUSR2, &sa, NULL);
-	/*while (len > count)
-		ft_killed(argv[2][count++], pid);*/
+
+	ft_printf("\n");
+	while (len > count)
+		ft_killed(argv[2][count++], pid);
+	//ft_send_str(pid, argv[2]);
 	return (1);
 }
--- server_str.c
+++ server_str.c
@@ -6,7 +6,7 @@
 /*   By: tvillare <tvillare@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/22 15:43:38 by tvillare          #+#    #+#             */
-/*   Updated: 2023/01/23 15:43:17 by tvillare         ###   ########.fr       */
+/*   Updated: 2023/01/23 18:56:29 by tvillare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,23 +21,24 @@ static void	viw_pid(void)
 	pid = getpid();
 	ft_printf("pid: %d\n", pid);
 }
-//modificar para invertir guardar los datos
+
 static void	sig_len(int signo, siginfo_t *info, void *context)
 {
-	static unsigned long long	elevar = 1;
-	static int	i = 1;
-
-
+	static  long	elevar = 1;
+	static int		i = 1;
+	ft_printf("elevator->%d:%d->%d\n", i++, elevar, g_data.len_str);
 	if (SIGUSR1 == signo)
 	{
+		ft_printf("(1)");
 		g_data.len_str |= elevar;
 		elevar *=2;
 	}
 	else if (SIGUSR2 == signo)
 	{
+		ft_printf("(0)");
 		elevar *=2;
 	}
-	ft_printf("elevator->%d:%d->%d\n", i++, elevar, g_data.len_str);
+
 	//if (kill(info->si_pid, SIGUSR1) == -1)
 		//exit(EXIT_FAILURE);
 	//if (elevar > 1073741824 ) //2^31
@@ -48,18 +49,19 @@ static void	sig_len(int signo, siginfo_t *info, void *context)
 		i = 1;
 		//g_data.len_str = 0;
 	}
-	//poner if fin de numero y setear el valor elebar en 1
 }
+
 static void	sig_usr(int signo, siginfo_t *info, void *context)
 {
-	static int	bit;
+	static int	bit = 0;
 	static char	letter;
-	static char	index;
+	static int	index = 0;
 
 	(void)context;
 	if (signo == SIGUSR1)
 	{
 		//kill(info->si_pid, SIGUSR2);
+		ft_printf("<1>");
 		letter = letter << 1;
 		letter |= 1;
 		bit++;
@@ -67,14 +69,22 @@ static void	sig_usr(int signo, siginfo_t *info, void *context)
 	else if (signo == SIGUSR2)
 	{
 		//kill(info->si_pid, SIGUSR2);
+		ft_printf("<0>");
 		bit++;
 		letter = letter << 1;
 	}
-	if (8 <= bit)
+	if (bit % 8 == 0)
 	{
-		bit = 0;
+		ft_printf("%c\n", letter);
+		//bit = 0;
 		g_data.str[index++] = letter;
-		letter &= 0;
+		if (index > g_data.len_str - 1)
+		{
+			index = 0;
+			bit = 0;
+			ft_printf("\nFIN str\n");
+		}
+		letter = 0;
 	}
 }
 
@@ -91,12 +101,10 @@ static void	len_str(int bit)
 		ft_printf("can not catch SIGUSR2\n");
 	while (bit++ < 31)
 	{
-		ft_printf("%d\n", bit);
+		ft_printf("/%d/\n", bit);
 		pause();
 	}
 	ft_printf("//sig_len:%d\n", g_data.len_str);
-	//bit = 0;
-	//ft_calloc(sizeof(g_data.str), g_data.len_str);
 }
 
 static void	len_letter(void)
@@ -105,33 +113,98 @@ static void	len_letter(void)
 	int					i;
 
 	i = 0;
+
 	sa.sa_flags = SA_SIGINFO;
 	sa.sa_sigaction = sig_usr;
 	if (sigaction(SIGUSR1, &sa, NULL) == -1)
 		ft_printf("can not catch SIGUSR1\n");
 	if (sigaction(SIGUSR2, &sa, NULL) == -1)
 		ft_printf("can not catch SIGUSR2\n");
+	while (++i <= (g_data.len_str) * 8)
+	{
+		ft_printf("&%d&", i%9);
+		pause();
+	}
+}
+
+
+
+
+
+
+
+/*
+static void	ft_sig_handler_str(int sig, siginfo_t *info, void *ucontext)
+{
+	static int	i = 0;
+	static int	j = -1;
+	static int	c;
+
+	(void)ucontext;
+	if (sig == SIGUSR1)
+	{
+		c >>= 1;
+		c += 128;
+	}
+	else
+		c >>= 1;
+	if (++i % 8 == 0)
+	{
+		g_data.str[++j] = (char)c;
+		if ((char)c == '\0')
+		{
+			j = -1;
+		}
+	}
+	usleep(100);
+	//if (kill(info->si_pid, SIGUSR1) == -1)
+		//exit(EXIT_FAILURE);
+	return ;
+}
+
+
+static void	ft_receive_str()
+{
+	int					i;
+	struct sigaction	sa_char;
+
+	g_data.str = malloc(sizeof(char) * (g_data.len_str + 1));
+	if (!g_data.str)
+		exit(EXIT_FAILURE);
+	sa_char.sa_flags = SA_SIGINFO;
+	sa_char.sa_sigaction = &ft_sig_handler_str;
+	sigaction(SIGUSR1, &sa_char, NULL);
+	sigaction(SIGUSR2, &sa_char, NULL);
+	i = 0;
 	while (++i <= (g_data.len_str + 1) * 8)
 	{
 		pause();
 	}
-	ft_calloc(sizeof(g_data.str), g_data.len_str);
+	return ;
 }
+*/
 
 int	main(void)
 {
 	int	bit;
 
+	g_data.len_str = 0;
 	bit = 0;
 	viw_pid();
 	while (1)
 	{
 		len_str(bit);
-		//len_letter();
-		ft_printf("--------------------");
-		ft_printf("%s\n", g_data.str);
+		//usleep(10);
+		ft_printf("--------------------\n");
+		//ft_printf("%s\n", g_data.str);
+		g_data.str = ft_calloc(sizeof(char), g_data.len_str + 1);
+		len_letter();
+		//ft_receive_str();
 		g_data.len_str = 0;
 		bit = 1;
+		ft_printf("\n%s/a/\n", g_data.str);
+		free(g_data.str);
+		ft_printf("\nFIN\n");
 		pause();
 	}
 
